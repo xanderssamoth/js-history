0d08408d3cd01a7050735d00bdc1e07980f33a2f
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2024-12-31 09:29:50 +0100

    Update history-basics-ajax.blade.php

---
+++ resources/views/partials/history-basics-ajax.blade.php
@@ -0,0 +1,21 @@
+
+        <div class="mb-4">
+            <div class="px-5 text-center bg-body-tertiary">
+                <div class="container py-4">
+                    <h1 class="display-1"><i class="bi bi-5-circle"></i></h1>
+                    <h1 class="text-body-emphasis">History API Basics with Ajax</h1>
+                    <p class="col-lg-8 lead mx-auto my-4">
+                        The History API enables a website to interact with the browser's session history: that is, the list of pages that the user has visited in a given window.
+                    </p>
+                    <a href="{{ route('home') }}" class="btn btn-lg btn-light shadow-0">Back home</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-lg container-fluid mb-4">
+            <div class="row mb-4">
+                <div class="col-lg-8 col-sm-11 col-12 mx-auto">
+
+                </div>
+            </div>
+        </div>
